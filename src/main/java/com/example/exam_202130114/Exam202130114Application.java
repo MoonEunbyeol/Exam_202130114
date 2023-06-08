fb238c9c71659324211da063eab999affb57ed46
MoonEunbyeol <202130114@email.daelim.ac.kr>
2023-06-08 14:22:56 +0900

    init

---
+++ src/main/java/com/example/exam_202130114/Exam202130114Application.java
@@ -0,0 +1,13 @@
+package com.example.exam_202130114;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exam202130114Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exam202130114Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exam_202130114/Exam202130114ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exam_202130114;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exam202130114ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
